0a0ddf174378ef26db49f2a31ec65f8f22b0f9ad
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-16 16:50:50 -0600

    Demo ProgressBar in App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Bar {
+  progress: number;
+  baseColor: string;
+  barColor: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +14,41 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title: string = 'PENNY FE DEV TEST';
   loading: boolean = false;
+  autoBar: Bar = {
+    progress: 10,
+    baseColor: 'rgba(0, 0, 255, 0.25)',
+    barColor: 'blue',
+  };
+  bars: Bar[] = [
+    {
+      progress: 0,
+      baseColor: 'rgba(255, 0, 0, 0.25)',
+      barColor: 'red',
+    },
+    {
+      progress: 50,
+      baseColor: 'rgba(220, 220, 56, 0.25)',
+      barColor: 'rgb(220, 220, 56)',
+    },
+    {
+      progress: 100,
+      baseColor: 'rgba(0, 255, 0, 0.25)',
+      barColor: 'green',
+    },
+  ];
 
   ngOnInit() {
     this.loading = true;
+    this.autoBarDemo();
     setTimeout(() => (this.loading = false), this.randomInt(4, 8) * 1000);
+  }
+
+  // Bar increases from 10% to 90% on repeat
+  autoBarDemo() {
+    setInterval(() => {
+      this.autoBar.progress =
+        this.autoBar.progress >= 90 ? 10 : this.autoBar.progress + 10;
+    }, 800);
   }
 
   randomInt(min: number, max: number) {
